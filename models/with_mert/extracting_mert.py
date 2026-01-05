0caf46dee67741b77456dcc3c5663c3494eae36f
Kiryl Sankouski <sankovsky85@gmail.com>
2026-01-05 12:08:34 +0100

    Added MERT model

---
+++ models/with_mert/extracting_mert.py
@@ -0,0 +1,82 @@
+import os
+import numpy as np
+import librosa
+import torch
+from transformers import Wav2Vec2FeatureExtractor, AutoModel
+import pandas as pd
+import glob
+from tqdm import tqdm
+
+DATA_PATH = '../../data'
+SAVE_DIR = '../../mert_features/'
+SR_MERT = 24000 #Hgz
+WINDOW_SEC = 3.0
+HOP_SEC = 1.5
+
+os.makedirs(SAVE_DIR, exist_ok=True)
+
+print("Loading MERT model...")
+model_name = "m-a-p/MERT-v1-95M"
+processor = Wav2Vec2FeatureExtractor.from_pretrained(model_name)
+mert_model = AutoModel.from_pretrained(model_name, trust_remote_code=True)
+
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+mert_model.to(device)
+mert_model.eval()
+
+
+def extract_windows_features(file_path):
+    audio, _ = librosa.load(file_path, sr=SR_MERT)
+    samples_per_window = int(WINDOW_SEC * SR_MERT)
+    samples_per_hop = int(HOP_SEC * SR_MERT)
+    embeddings = []
+
+    for start in range(0, len(audio) - samples_per_window, samples_per_hop):
+        window = audio[start: start + samples_per_window]
+        inputs = processor(window, sampling_rate=SR_MERT, return_tensors="pt").to(device)
+
+        with torch.no_grad():
+            outputs = mert_model(**inputs, output_hidden_states=True)
+
+        feat_max = torch.max(outputs.last_hidden_state, dim=1).values
+        feat_mean_early = torch.mean(outputs.hidden_states[1], dim=1)
+        combined = torch.cat((feat_max, feat_mean_early), dim=1)
+        embeddings.append(combined.squeeze().cpu().numpy())
+
+    return embeddings
+
+
+def main():
+    file_paths = glob.glob(os.path.join(DATA_PATH, "**/*.wav"), recursive=True)
+    data_log = []
+
+    for path in tqdm(file_paths, desc="Progress"):
+        filename = os.path.basename(path)
+
+        if 'sect1' in filename:
+            label = 1
+        elif 'sect0' in filename:
+            label = 0
+        else:
+            continue
+
+        try:
+            window_embs = extract_windows_features(path)
+            for i, emb in enumerate(window_embs):
+                feat_filename = f"{filename}_win_{i}.npy"
+                save_path = os.path.join(SAVE_DIR, feat_filename)
+                np.save(save_path, emb)
+                data_log.append({'mert_path': save_path, 'label': label})
+        except Exception as e:
+            print(f"Error: {e}")
+
+    if data_log:
+        result_df = pd.DataFrame(data_log)
+        result_df.to_csv('../../mert_dataset_map.csv', index=False)
+        print("Saved!")
+    else:
+        print("No files found")
+
+
+if __name__ == "__main__":
+    main()
---
+++ models/with_mert/train_mert_model.py
@@ -0,0 +1,84 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers, models, callbacks
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+from sklearn.utils import class_weight
+
+
+MERT_MAP_PATH = '../../mert_dataset_map.csv'
+MODEL_NAME = 'vinyl_mert_classifier.keras'
+BATCH_SIZE = 32
+EPOCHS = 60
+
+
+def load_mert_data(csv_path):
+    df = pd.read_csv(csv_path)
+    X = []
+    for path in df['mert_path']:
+        X.append(np.load(path))
+    return np.array(X).astype('float32'), df['label'].values.astype('float32')
+
+
+def train():
+    X, y = load_mert_data(MERT_MAP_PATH)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    # 1536 input size from MERT
+    model = models.Sequential([
+        layers.Input(shape=(1536,)),
+
+        layers.Dense(512, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Dropout(0.4),
+
+        layers.Dense(256, activation='relu'),
+        layers.BatchNormalization(),
+        layers.Dropout(0.3),
+
+        layers.Dense(128, activation='relu'),
+        layers.Dense(1, activation='sigmoid')
+    ])
+
+    optimizer = tf.keras.optimizers.Adam(learning_rate=0.0001)
+
+    model.compile(
+        optimizer=optimizer,
+        loss='binary_crossentropy',
+        metrics=['accuracy', tf.keras.metrics.Recall(name='recall')]
+    )
+
+    weights = class_weight.compute_class_weight('balanced', classes=np.unique(y_train), y=y_train)
+    class_weights = dict(enumerate(weights))
+
+    сallbacks = [
+        callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True),
+
+        callbacks.ModelCheckpoint(MODEL_NAME, monitor='val_accuracy', save_best_only=True)
+    ]
+
+
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_test, y_test),
+        epochs=EPOCHS,
+        batch_size=BATCH_SIZE,
+        class_weight=class_weights,
+        callbacks=сallbacks,
+        verbose=1
+    )
+
+
+    plt.figure(figsize=(10, 4))
+    plt.plot(history.history['accuracy'], label='Train Acc')
+    plt.plot(history.history['val_accuracy'], label='Val Acc')
+    plt.title('Training Accuracy')
+    plt.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    train()
